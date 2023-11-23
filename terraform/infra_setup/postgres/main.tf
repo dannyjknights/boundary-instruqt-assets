8c736bcd7b5cd839404e2d30acc0047746946a86
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2023-11-23 11:07:42 +0000

    Update main.tf

--- terraform/infra_setup/postgres/main.tf
+++ terraform/infra_setup/postgres/main.tf
@@ -10,8 +10,8 @@ locals {
   cloudinit_config_postgres = {
     write_files = [
       {
-        //content     = file("${path.root}/files/gpg_pubkeys/hashicorp-archive-keyring.gpg")
-        content     = file("../files/gpg_pubkeys/hashicorp-archive-keyring.gpg")
+        content     = file("${path.root}/files/gpg_pubkeys/hashicorp-archive-keyring.gpg")
+        //content     = file("../files/gpg_pubkeys/hashicorp-archive-keyring.gpg")
         owner       = "root:root"
         path        = "/tmp/hashicorp-archive-keyring.gpg"
         permissions = "0644"
